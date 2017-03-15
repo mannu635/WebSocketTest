bf3b2032769d0b4ecac232757625c688f0064af7
Ilia <Ilia@WORKLIO-CA-IC>
2017-03-15 17:56:00 -0400

    clean up

--- AspNetChat/WebSocketChatHandler.cs
+++ AspNetChat/WebSocketChatHandler.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Web;
+﻿using System.Collections.Generic;
 using Microsoft.Web.WebSockets;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace AspNetChat
@@ -22,15 +16,15 @@ namespace AspNetChat
       this.name = this.WebSocketContext.QueryString["username"];
       if (name == "") { name = "NoName"; }
       clients.Add(this);
-      
+
       _storeMessages = Task.Run(() => _service.GetAll()).Result;
-      foreach( var message in _storeMessages)
-            {
-                this.Send(string.Format("<span style='color:grey;font-size: 9px;'>{0} : {1}</span>", message.Name, message.Text));
-            }
-            this.Send("<hr/>");
-            clients.Broadcast(string.Format("<strong><small> {0} joined. </small></strong>", name));
-        }
+      foreach (var message in _storeMessages)
+      {
+        this.Send(string.Format("<span style='color:grey;font-size: 9px;'>{0} : {1}</span>", message.Name, message.Text));
+      }
+      this.Send("<hr/>");
+      clients.Broadcast(string.Format("<strong><small> {0} joined. </small></strong>", name));
+    }
 
     public override void OnMessage(string message)
     {
@@ -40,7 +34,7 @@ namespace AspNetChat
 
     public override void OnClose()
     {
-      clients.Remove(this); 
+      clients.Remove(this);
       clients.Broadcast(string.Format("<small style='color:red;font-size: 9px;'>{0} left.</small>", name));
     }
   }
